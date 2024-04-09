11b8cf1f82786c89513567dc425abffa2e81be71
unknown <ngcebenhleshabangu@gmail.com>
2024-04-09 12:19:31 +0200

    Deployed version

--- src/Componets/LogIn.js
+++ src/Componets/LogIn.js
@@ -1,73 +1,67 @@
-import React, {useEffect, useState} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import {login} from '../Action/UserActions'
-import './LogIn.css'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../Action/UserActions";
+import "./LogIn.css";
 
-import {openModal} from '../Action/ModalAction'
-
-
+import { openModal } from "../Action/ModalAction";
 
 const LogIn = () => {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const dispatch = useDispatch();
 
-  const userLogin = useSelector(state => state.userLogin)
-  const { loading, error, userInfo} = userLogin;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
 
-  useEffect(() =>{
-    if(userInfo){
-       dispatch(openModal('closed',""))
+  useEffect(() => {
+    if (userInfo) {
+      dispatch(openModal("closed", ""));
     }
-  },[userInfo])
+  }, [userInfo]);
 
-    const submitLogin = (e) =>{
-      e.preventDefault()
-      dispatch(login(email,password))
-    }
+  const submitLogin = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
   return (
+    <div className="login-form">
+      <h2>Log In or Sign Up</h2>
 
+      {error && <h2>{error}</h2>}
+      {loading && <h2>loading...</h2>}
+      <form onSubmit={submitLogin}>
+        <button className="facebook-login">Connect with Facebook</button>
+        <button className="Google-login">Connect with Google</button>
 
-    <div className='login-form'>
-        <h2>Log In or Sign Up</h2>
+        <div className="login-or">
+          <span>Or</span>
 
-        {error&& <h2>{error}</h2>}
-        {loading&&<h2>loading...</h2>}
-        <form onSubmit={submitLogin}>
-           <button className='facebook-login'>Connect with Facebook</button>
-           <button className='Google-login'>Connect with Google</button>
-           
-           <div className='login-or'>
-                 <span>Or</span>
+          <div className="or-divider"> </div>
+        </div>
 
-                 <div className='or-divider'> </div>  
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="browser-default"
+          placeholder="Email address"
+        />
 
-           </div>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="browser-default"
+          placeholder="Password"
+        />
 
-           <input
-                  type='email'
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className='browser-default'
-                  placeholder='Email address'
-                  />
-
-          <input
-                 type='password'
-                 value={password}
-                 onChange={(e) => setPassword(e.target.value)}
-                 className='browser-default'
-                 placeholder='Password'
-                  />  
-
-         <button className='sign-up-button'>Login</button> 
-         <div className='divider'></div>
-         <div>Don't have an Account? Sign up</div>
-        </form>
+        <button className="sign-up-button">Login</button>
+        <div className="divider"></div>
+        <div>Don't have an Account? Sign up</div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LogIn
+export default LogIn;
